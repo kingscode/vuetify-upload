6fefeeb5d95603b8f7ee5e7a759cf4fd1e039292
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-02-24 09:56:12 +0100

    added a playground

---
+++ playground/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-container fluid fill-height>
+            <vuetify-upload
+                label="Images for the slideshow"
+                :max="4"
+                can-delete
+                accept="image/*"
+                color="accent"
+            ></vuetify-upload>
+        </v-container>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+
+</style>
